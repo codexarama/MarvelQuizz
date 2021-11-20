946b109d167c961753d33d787f35e8602f5fe1b5
Amandine Arias <amandine.arias@gmail.com>
2021-11-20 20:37:43 +0100

    feat (Register) : create component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 export default function Register() {
   return (
-    <div className="signUpLoginBox">
-      <div className="slContainer">Register</div>
-    </div>
+    <main>
+      <div className="signUpLoginBox">
+        <div className="slContainer">
+          <div className="formBoxLeftSignup"></div>
+          <div className="formBoxRight">
+            <div className="formContent">
+              <h2>Inscription</h2>
+              <form action="">
+                <div className="inputBox">
+                  <label htmlFor="pseudo">Pseudo</label>
+                  <input type="text" id="pseudo" required />
+                </div>
+                <div className="inputBox">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" id="email" required />
+                </div>
+                <div className="inputBox">
+                  <label htmlFor="password">Mot de passe</label>
+                  <input type="password" id="password" required />
+                </div>
+                <div className="inputBox">
+                  <label htmlFor="confirmPassword">
+                    Confirmer le mot de passe
+                  </label>
+                  <input type="password" id="confirmPassword" required />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
